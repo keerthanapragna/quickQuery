688a963d9e550bd13692e829dfed6ba9453302fe
SabaBegum2428 <saba55443322@gmail.com>
2025-08-29 15:57:28 +0530

    added js file

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn=document.querySelector(".log");
+btn.addEventListener("click",()=>{
+    window.location.href="login.html";
+})
+let bt=document.querySelector(".sign");
+bt.addEventListener("click",()=>{
+    window.location.href="signin.html";
+})
